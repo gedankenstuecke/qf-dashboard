7d67f8dfb0629b4a9e7dbca7c11fa8a01baaa87d
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2020-05-25 21:19:57 +0200

    add project id

--- qf_dashboard/settings.py
+++ qf_dashboard/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+OH_PROJECT_ID = os.getenv("OH_PROJECT_ID")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
