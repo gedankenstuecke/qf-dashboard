a0ba49ff5b115eceb6dcf10a4ab8be5f69def4f8
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2020-05-25 21:17:28 +0200

    .gitignore added

--- main/management/commands/update_members.py
+++ main/management/commands/update_members.py
@@ -1,14 +1,14 @@
 from django.core.management.base import BaseCommand
 from main.models import Member
 import requests
-
+from django.conf import settings
 
 class Command(BaseCommand):
     help = 'Process so far unprocessed data sets'
 
     def handle(self, *args, **options):
         #how to keep the heroku app alive despite a free dyno :joy:
-        response = requests.get('https://www.openhumans.org/api/public-data/members-by-source/?source=direct-sharing-39')
+        response = requests.get('https://www.openhumans.org/api/public-data/members-by-source/?source={}'.format(settings.OH_PROJECT_ID))
         response_members = response.json()['results'][0]['usernames']
         for response_member in response_members:
             member, _ = Member.objects.get_or_create(member_name=response_member)
